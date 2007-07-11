a0160cc763a574bb87ebfe0c03eff526b37d420d
dhanji <dhanji@28d35608-eb31-0410-8cd1-8f5b667a57d6>
2007-07-11 04:07:33 +0000

    javadoc comments added
    git-svn-id: http://warp-persist.googlecode.com/svn/trunk/warp-persist@34 28d35608-eb31-0410-8cd1-8f5b667a57d6

--- src/com/wideplay/warp/persist/TransactionStrategyBuilder.java
+++ src/com/wideplay/warp/persist/TransactionStrategyBuilder.java
@@ -10,9 +10,31 @@ import com.google.inject.matcher.Matcher;
  * @since 1.0
  */
 public interface TransactionStrategyBuilder extends PersistenceModuleBuilder {
+    /**
+     *
+     * @param transactionStrategy One of the enum TransactionStrategy (typically LOCAL or JTA).
+     *  Note that JTA requires configuring Hibernate to use the JEE container's transaction implementation.
+     *  See warp-persist website for details.
+     * @return Returns the next step in the builder chain
+     */
     TransactionStrategyBuilder transactedWith(TransactionStrategy transactionStrategy);
 
+    /**
+     *
+     * @param classMatcher A Matcher on classes to watch for @Transactional methods
+     * @return Returns the next step in the builder chain
+     */
     PersistenceModuleBuilder forAll(Matcher<? super Class<?>> classMatcher);
 
+    /**
+     *
+     * @param daoInterface An interface with some methods (typically all) that are
+     *  annotated with @Finder(..). Warp-persist will automagically create method bodies
+     *  and provide an instance when you inject this interface anywhere in your setup.
+     *  Note that you are *not* allowed to provide your own implementation/binding for such
+     *  interfaces.
+     *
+     * @return Returns the next step in the builder chain
+     */
     TransactionStrategyBuilder addAccessor(Class<?> daoInterface);
 }
