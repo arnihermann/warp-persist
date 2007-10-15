50ba0e9f984f7db00685bf94e6eaa7fbb0c27900
dhanji <dhanji@28d35608-eb31-0410-8cd1-8f5b667a57d6>
2007-10-15 06:43:57 +0000

    more tests for SPR filter and custom method matchers for txn support
    git-svn-id: http://warp-persist.googlecode.com/svn/trunk/warp-persist@58 28d35608-eb31-0410-8cd1-8f5b667a57d6

---
+++ test/com/wideplay/warp/hibernate/SessionPerRequestFilterTest.java
@@ -0,0 +1,19 @@
+package com.wideplay.warp.hibernate;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dhanji
+ * Date: Oct 10, 2007
+ * Time: 4:56:32 PM
+ *
+ * @author Dhanji R. Prasanna (dhanji gmail com)
+ */
+public class SessionPerRequestFilterTest {
+
+    @Test
+    public final void testDoFilter() {
+        
+    }
+}
---
+++ test/com/wideplay/warp/jpa/ManualLocalTransactionsWithCustomMatcherTest.java
@@ -0,0 +1,104 @@
+package com.wideplay.warp.jpa;
+
+import com.google.inject.Injector;
+import com.google.inject.Guice;
+import com.google.inject.AbstractModule;
+import com.google.inject.Inject;
+import com.google.inject.matcher.Matchers;
+import com.wideplay.warp.persist.PersistenceService;
+import com.wideplay.warp.persist.UnitOfWork;
+import com.wideplay.warp.persist.TransactionStrategy;
+import com.wideplay.warp.persist.Transactional;
+
+import java.util.Date;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityManager;
+
+/**
+ * Created with IntelliJ IDEA.
+ * On: 2/06/2007
+ *
+ * For instance, a session-per-request strategy will control the opening and closing of the EM
+ * at its own (manual) discretion. As opposed to a transactional unit of work.
+ *
+ * @author Dhanji R. Prasanna <a href="mailto:dhanji@gmail.com">email</a>
+ * @since 1.0
+ */
+public class ManualLocalTransactionsWithCustomMatcherTest {
+    private Injector injector;
+    private static final String UNIQUE_TEXT = "some unique text" + new Date();
+    private static final String UNIQUE_TEXT_2 = "some other unique text" + new Date();
+
+    @BeforeClass
+    public void pre() {
+        injector = Guice.createInjector(PersistenceService.usingJpa()
+            .across(UnitOfWork.REQUEST)
+            .transactedWith(TransactionStrategy.LOCAL)
+            .forAll(Matchers.subclassesOf(TransactionalObject.class), Matchers.any())
+            .buildModule(),
+                new AbstractModule() {
+
+                    protected void configure() {
+                        //tell Warp the name of the jpa persistence unit
+                        bindConstant().annotatedWith(JpaUnit.class).to("testUnit");
+                    }
+                });
+
+        //startup persistence
+        injector.getInstance(PersistenceService.class)
+                .start();
+    }
+
+    @AfterClass
+    public void postClass() {
+        injector.getInstance(EntityManagerFactory.class).close();
+    }
+
+    @Test
+    public void testSimpleCrossTxnWork() {
+        //pretend that the request was started here
+        EntityManager em = injector.getInstance(EntityManager.class);
+
+        JpaTestEntity entity = injector.getInstance(ManualLocalTransactionsWithCustomMatcherTest.TransactionalObject.class).runOperationInTxn();
+        injector.getInstance(ManualLocalTransactionsWithCustomMatcherTest.TransactionalObject.class).runOperationInTxn2();
+
+        //persisted entity should remain in the same em (which should still be open)
+        assert injector.getInstance(EntityManager.class).contains(entity) : "EntityManager  appears to have been closed across txns!";
+        assert em.contains(entity) : "EntityManager  appears to have been closed across txns!";
+        assert em.isOpen() : "EntityManager appears to have been closed across txns!";
+
+        EntityManagerFactoryHolder.closeCurrentEntityManager();
+
+        //try to query them back out
+        em = injector.getInstance(EntityManager.class);
+        assert null != em.createQuery("from JpaTestEntity where text = :text").setParameter("text", UNIQUE_TEXT).getSingleResult();
+        assert null != em.createQuery("from JpaTestEntity where text = :text").setParameter("text", UNIQUE_TEXT_2).getSingleResult();
+        em.close();
+    }
+
+
+    public static class TransactionalObject {
+        @Inject
+        EntityManager em;
+
+        public JpaTestEntity runOperationInTxn() {
+            JpaTestEntity entity = new JpaTestEntity();
+            entity.setText(UNIQUE_TEXT);
+            em.persist(entity);
+
+            return entity;
+        }
+
+        public void runOperationInTxn2() {
+            JpaTestEntity entity = new JpaTestEntity();
+            entity.setText(UNIQUE_TEXT_2);
+            em.persist(entity);
+        }
+
+    }
+}
