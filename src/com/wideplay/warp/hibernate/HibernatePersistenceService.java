3a32622d90373277e76e47cae110575ea3f4b4d5
robbie.vanbrabant <robbie.vanbrabant@28d35608-eb31-0410-8cd1-8f5b667a57d6>
2008-08-19 19:56:45 +0000

    Removed unused annotation
    git-svn-id: http://warp-persist.googlecode.com/svn/trunk/warp-persist@98 28d35608-eb31-0410-8cd1-8f5b667a57d6

--- src/com/wideplay/warp/hibernate/HibernatePersistenceService.java
+++ src/com/wideplay/warp/hibernate/HibernatePersistenceService.java
@@ -16,15 +16,10 @@
 
 package com.wideplay.warp.hibernate;
 
-import com.google.inject.BindingAnnotation;
 import com.google.inject.Inject;
 import com.wideplay.warp.persist.PersistenceService;
-import org.hibernate.cfg.Configuration;
-
-import java.lang.annotation.Retention;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
-
 import net.jcip.annotations.Immutable;
+import org.hibernate.cfg.Configuration;
 
 /**
  * Created with IntelliJ IDEA.
@@ -52,11 +47,6 @@ class HibernatePersistenceService extends PersistenceService {
         //if necessary, set the JNDI lookup name of the JTA txn
         HibernateJtaTxnInterceptor.setUserTransactionJndiName(configuration.getProperty(JTA_USER_TRANSACTION));
     }
-
-    @BindingAnnotation
-    @Retention(RUNTIME)
-    public @interface PersistenceProperties { }
-
 
     @Override
     public boolean equals(Object obj) {
