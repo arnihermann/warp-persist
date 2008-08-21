715082a5029217248896afebec16a5ecde97bdcd
robbie.vanbrabant <robbie.vanbrabant@28d35608-eb31-0410-8cd1-8f5b667a57d6>
2008-08-21 18:46:03 +0000

    Tests for the SessionPerRequestFilter that we will reuse for other persistence strategies.
    git-svn-id: http://warp-persist.googlecode.com/svn/trunk/warp-persist@111 28d35608-eb31-0410-8cd1-8f5b667a57d6

--- test/com/wideplay/warp/hibernate/HibernateSPRFilterTest.java
+++ test/com/wideplay/warp/hibernate/HibernateSPRFilterTest.java
@@ -16,47 +16,94 @@
 
 package com.wideplay.warp.hibernate;
 
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.matcher.Matchers;
+import com.wideplay.codemonkey.web.startup.Initializer;
+import com.wideplay.warp.persist.PersistenceService;
+import com.wideplay.warp.persist.TransactionStrategy;
+import com.wideplay.warp.persist.UnitOfWork;
+import com.wideplay.warp.persist.WorkManager;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.context.ManagedSessionContext;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import java.io.IOException;
 
-/**
- * Created by IntelliJ IDEA.
- * User: Dhanji R. Prasanna (dhanji@gmail.com)
- * Date: 11/10/2007
- * Time: 11:16:17
- * <p/>
- * TODO: Describe me!
- *
- * @author Dhanji R. Prasanna (dhanji@gmail.com)
- * @since 1.0
- */
+/** Unit tests the SPR filter. */
 public class HibernateSPRFilterTest {
+    private Injector injector;
+    
+    @BeforeClass
+    public void pre() {
+        injector = Guice.createInjector(PersistenceService.usingHibernate()
+            .across(UnitOfWork.REQUEST)
+            .transactedWith(TransactionStrategy.LOCAL)
+            .forAll(Matchers.any())
+            .buildModule(),
+                new AbstractModule() {
+                    protected void configure() {
+                        bind(Configuration.class).toInstance(new AnnotationConfiguration()
+                            .addAnnotatedClass(HibernateTestEntity.class)
+                            .setProperties(Initializer.loadProperties("spt-persistence.properties")));
+                    }
+                });
 
-//    @Test
-    //TODO this is a bit difficult to test--how do I setup mocks for static APIs within Hibernate =(
-    public final void testDoFilter() throws IOException, ServletException {
-        throw new Error("unimplementated");
-//        //create our spr filter and set it up
-//        SessionPerRequestFilter filter = new SessionPerRequestFilter();
-//        SessionPerRequestFilter.setUnitOfWork(UnitOfWork.REQUEST);
-//
-//
-//        SessionFactory mockSF = createNiceMock(SessionFactory.class);
-//        expect(mockSF.getCurrentSession()).andReturn(createNiceMock(Session.class));
-//
-//        new SessionFactoryHolder().setSessionFactory(mockSF);
-//
-//        //create our mock servlet context artifacts
-//        FilterChain filterChain = createStrictMock(FilterChain.class);
-//        ServletRequest mockRequest = createStrictMock(ServletRequest.class);
-//        ServletResponse mockResponse = createStrictMock(ServletResponse.class);
-//
-//        //prepare mocks for battle!
-//        replay(filterChain, mockRequest, mockResponse, mockSF);
-//
-//        //test filter
-//        filter.doFilter(mockRequest, mockResponse, filterChain);
-//
-//        verify(filterChain);
+        //startup persistence
+        injector.getInstance(PersistenceService.class).start();
+    }
+
+    @AfterClass
+    public void post() {
+        injector.getInstance(SessionFactory.class).close();
+    }
+
+    @Test
+    public final void testUseWorkManager() throws IOException, ServletException {
+        SessionPerRequestFilter spr = new SessionPerRequestFilter();
+        final ValidatableWorkManager workManager = new ValidatableWorkManager();
+        SessionPerRequestFilter.registerWorkManager(workManager);
+        FilterChain chain = new FilterChain() {
+            public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse) throws IOException, ServletException {
+                assert workManager.beginCalled;
+                assert !workManager.endCalled;
+            }
+        };
+        spr.doFilter(null, null, chain);
+        assert workManager.beginCalled;
+        assert workManager.endCalled;
+    }
+
+    @Test
+    public final void testUseRealWorkManager() throws IOException, ServletException {
+        SessionPerRequestFilter spr = new SessionPerRequestFilter();
+        SessionPerRequestFilter.registerWorkManager(injector.getInstance(WorkManager.class));
+        FilterChain chain = new FilterChain() {
+            public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse) throws IOException, ServletException {
+                assert ManagedSessionContext.hasBind(injector.getInstance(SessionFactory.class));
+            }
+        };
+        spr.doFilter(null, null, chain);
+        assert !ManagedSessionContext.hasBind(injector.getInstance(SessionFactory.class));
+    }
+
+    static class ValidatableWorkManager implements WorkManager {
+        boolean beginCalled;
+        boolean endCalled;
+        public void beginWork() {
+            beginCalled = true;
+        }
+        public void endWork() {
+            endCalled = true;
+        }
     }
 }
